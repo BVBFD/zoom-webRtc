d202168ec8e5416c09aac2c52402f37793020d8e
BVBFD <lsevina126@gmail.com>
2022-12-20 18:32:18 +0900

    add nickname object json stringify on client side

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,7 +1,13 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
 // 클라이언트에서 socket은 서버를 의미함
+
+const makeMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
 
 const handleOpen = () => {
   console.log("Connected To Browser!!");
@@ -10,22 +16,32 @@ const handleOpen = () => {
 socket.addEventListener("open", handleOpen);
 
 socket.addEventListener("message", (message) => {
-  console.log("New message: ", message.data);
+  // console.log("New message: ", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("Disconnected from Server");
 });
 
-setTimeout(() => {
-  socket.send("hello from the browser!");
-}, 10000);
+// setTimeout(() => {
+//   socket.send("hello from the browser!");
+// }, 10000);
 
 const handleSubmit = (event) => {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   input.value = "";
 };
 
+const handleNickSubmit = (event) => {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+};
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
